c65b30f5309a4fb925ce0e5d6c8ef433661afc14
monoxide0184 <monoxide0184@gmail.com>
2012-09-12 01:05:16 +1000

    Basic empty mod

---
+++ common/monoxide/Lanterns/Lanterns.java
@@ -0,0 +1,17 @@
+package monoxide.Lanterns;
+
+import cpw.mods.fml.common.*;
+import cpw.mods.fml.common.Mod.*;
+import cpw.mods.fml.common.network.*;
+import cpw.mods.fml.common.event.*;
+
+@Mod(modid = "monoxide_Lanterns", name = "Lanterns", version = "1.0.0.1")
+@NetworkMod(clientSideRequired = true, serverSideRequired = false)
+public class Lanterns {
+
+	@Init
+	public void Initialise(FMLInitializationEvent event) {
+		
+	}
+	
+}
