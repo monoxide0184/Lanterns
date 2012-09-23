784cc2bc1d37d46b34f33cfa36ded97eca2de711
monoxide <monoxide0184@gmail.com>
2012-09-24 01:19:25 +1000

    Initial 'interesting' particle effects.
    Probably need to tweak the velocity parameters. Seems that there's a
    built-in movement associated with particles.

--- common/monoxide/Lanterns/BlockZeroTorch.java
+++ common/monoxide/Lanterns/BlockZeroTorch.java
@@ -2,6 +2,10 @@ package monoxide.Lanterns;
 
 import java.util.Collection;
 import java.util.Dictionary;
+import java.util.Random;
+
+import cpw.mods.fml.common.Side;
+import cpw.mods.fml.common.asm.SideOnly;
 
 import net.minecraft.src.Block;
 import net.minecraft.src.BlockTorch;
@@ -43,6 +47,42 @@ public class BlockZeroTorch extends BlockTorch {
 		if (var7 != null)
 		{
 			var7.receiveClientEvent(par5, par6);
+		}
+	}
+	
+	@SideOnly(Side.CLIENT)
+	@Override
+	/**
+	 * Shamelessly ripped off from BlockTorch
+	 */
+	public void randomDisplayTick(World par1World, int par2, int par3, int par4, Random par5Random)
+	{
+		int var6 = par1World.getBlockMetadata(par2, par3, par4);
+		double var7 = (double)((float)par2 + 0.5F);
+		double var9 = (double)((float)par3 + 0.7F);
+		double var11 = (double)((float)par4 + 0.5F);
+		double var13 = 0.2199999988079071D;
+		double var15 = 0.27000001072883606D;
+		
+		if (var6 == 1)
+		{
+			par1World.spawnParticle("portal", var7 - var15, var9 + var13, var11, 0.0D, 0.0D, 0.0D);
+		}
+		else if (var6 == 2)
+		{
+			par1World.spawnParticle("portal", var7 + var15, var9 + var13, var11, 0.0D, 0.0D, 0.0D);
+		}
+		else if (var6 == 3)
+		{
+			par1World.spawnParticle("portal", var7, var9 + var13, var11 - var15, 0.0D, 0.0D, 0.0D);
+		}
+		else if (var6 == 4)
+		{
+			par1World.spawnParticle("portal", var7, var9 + var13, var11 + var15, 0.0D, 0.0D, 0.0D);
+		}
+		else
+		{
+			par1World.spawnParticle("portal", var7, var9, var11, 0.0D, 0.0D, 0.0D);
 		}
 	}
 }
