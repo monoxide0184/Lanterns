ea5c9ff3d166c08ff505c8b6d3fef3d8740465db
monoxide <monoxide0184@gmail.com>
2012-09-25 00:26:15 +1000

    Centralise version number handling in mcmod.info

--- src/common/monoxide/Lanterns/Lanterns.java
+++ src/common/monoxide/Lanterns/Lanterns.java
@@ -23,13 +23,13 @@ import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.registry.GameRegistry;
 import cpw.mods.fml.common.registry.LanguageRegistry;
 
-@Mod(modid = "monoxide_Lanterns", name = "Lanterns", version = Lanterns.VERSION)
+@Mod(modid = "monoxide_Lanterns")
 @NetworkMod(clientSideRequired = true, serverSideRequired = false)
 public class Lanterns {
 	// The instance of your mod that Forge uses.
 	@Instance("monoxide_Lanterns")
 	public static Lanterns instance;
-	public static final String VERSION = "1.0.0";
+	private static String version;
 	
 	// Says where the client and server 'proxy' code is loaded.
 	@SidedProxy(clientSide="monoxide.Lanterns.client.ClientProxy", serverSide="monoxide.Lanterns.CommonProxy")
@@ -40,8 +40,13 @@ public class Lanterns {
 	public static Block zeroTorch;
 	protected static int zeroTorchId;
 	
+	public static String getVersion() {
+		return version;
+	}
+	
 	@PreInit
 	public void PreInit(FMLPreInitializationEvent event) {
+		version = event.getModMetadata().version;
 		Configuration config = new Configuration(event.getSuggestedConfigurationFile());
 		
 		try {
